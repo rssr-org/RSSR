880db0be0954a09985f4f37815c05de7a1250b8a
ebrahimiaval <m.ebrahimiaval@gmail.com>
2020-02-07 17:23:00 +0330

    fix jsx support in server

--- provider/webpack/development.js
+++ provider/webpack/development.js
@@ -104,7 +104,7 @@ module.exports = [
         module: {
             rules: [
                 {
-                    test: /\.js$/,
+                    test: /\.(js|jsx)$/,
                     exclude: /(node_modules[\\\/])/,
                     use: [
                         "babel-loader",
--- provider/webpack/production.js
+++ provider/webpack/production.js
@@ -120,7 +120,7 @@ module.exports = [
         module: {
             rules: [
                 {
-                    test: /\.js$/,
+                    test: /\.(js|jsx)$/,
                     exclude: /(node_modules[\\\/])/,
                     use: [
                         {
