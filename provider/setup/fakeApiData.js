271b8c301e63db56fcb468bf3484f978bdfb55bb
ebrahimiaval <m.ebrahimiaval@gmail.com>
2020-03-09 01:11:28 +0330

    define fake API

---
+++ provider/setup/fakeApiData.js
@@ -0,0 +1,58 @@
+module.exports = {
+    "posts": [
+        {
+            "id": 0,
+            "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+            "body": "quia et suscipit. suscipit recusandae consequuntur expedita et cum. reprehenderit molestiae ut ut quas totam. nostrum rerum est autem sunt rem eveniet architecto"
+        },
+        {
+            "id": 1,
+            "title": "qui est esse",
+            "body": "est rerum tempore vitae. sequi sint nihil reprehenderit dolor beatae ea dolores neque. fugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis. qui aperiam non debitis possimus qui neque nisi nulla"
+        },
+        {
+            "id": 2,
+            "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+            "body": "et iusto sed quo iure. voluptatem occaecati omnis eligendi aut ad. voluptatem doloribus vel accusantium quis pariatur. molestiae porro eius odio et labore et velit aut"
+        },
+        {
+            "id": 3,
+            "title": "eum et est occaecati",
+            "body": "ullam et saepe reiciendis voluptatem adipisci. sit amet autem assumenda provident rerum culpa. quis hic commodi nesciunt rem tenetur doloremque ipsam iure. quis sunt voluptatem rerum illo velit"
+        },
+        {
+            "id": 4,
+            "title": "nesciunt quas odio",
+            "body": "repudiandae veniam quaerat sunt sed. alias aut fugiat sit autem sed est. voluptatem omnis possimus esse voluptatibus quis. est aut tenetur dolor neque"
+        },
+        {
+            "id": 5,
+            "title": "dolorem eum magni eos aperiam quia",
+            "body": "ut aspernatur corporis harum nihil quis provident sequi. mollitia nobis aliquid molestiae. perspiciatis et ea nemo ab reprehenderit accusantium quas. voluptate dolores velit et doloremque molestiae"
+        }
+    ],
+    "skeleton": {
+        "dailyMessage": "Be happy! Life is too short."
+    },
+    "signin": {
+        "token": "salkhfasoidpaskdpksapdksakdpisapdiasdphdpksapdksakdpisapdiasdphdpksapdksakdpisapdiasdphdioashdoihsaoid"
+    },
+    "signup": {
+        "token": "salkhfasoidpaskdpksapdksakdpisapdiasdphdpksapdksakdpisapdiasdphdpksapdksakdpisapdiasdphdioashdoihsaoid"
+    },
+    "userDetails": {
+        "firstName": "dan",
+        "lastName": "abramov",
+        "phone": "+123456789",
+        "email": "dan.abramov@gmail.com"
+    },
+    "forgetPassword": {
+        "message": "mail sent successfully. check your eamil."
+    },
+    "resetPasswordTrust": {
+        "message": "token is valid."
+    },
+    "resetPasswordSubmit": {
+        "message": "token is valid."
+    }
+}
