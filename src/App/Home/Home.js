02d7d5d435ee0f14bffef5b92c35a9cef7780f73
ebrahimiaval <m.ebrahimiaval@gmail.com>
2020-03-08 21:22:11 +0330

    remove  rssr-breakpoint and rssr-carousel

--- src/App/Home/Home.js
+++ src/App/Home/Home.js
@@ -4,63 +4,35 @@ import {Link} from "react-router-dom";
 import {api} from "../../setup/api";
 import {route} from "../../setup/route";
 import {fetcher} from "../../Partial/fetcher/fetcher";
-import Breakpoint from "rssr-breakpoint";
 import Namespace from "rssr-namespace";
 import {fetching} from "../../setup/utility/fetching";
 import "./home.scss";
 import SmartDirection from "rssr-smart-direction";
-import Carousel from "rssr-carousel";
 
 
 function Home(props) {
     const {homepage} = props;
     const sampleTextForSmartDirection = ['this is sample LTR text for test smart direction', 'نمونه متن راست چین برای ازمایش چینش هوشمند']
-    const sampleCarousel = [
-        '/asset/img/rssr-logo.png',
-        '/asset/img/rssr-logo.png',
-        '/asset/img/rssr-logo.png',
-        '/asset/img/rssr-logo.png',
-        '/asset/img/rssr-logo.png',
-        '/asset/img/rssr-logo.png',
-        '/asset/img/rssr-logo.png',
-        '/asset/img/rssr-logo.png'
-    ]
 
-    const carouselOptions = {
-        rightToLeft: true,
-        cellAlign: 'right',
-        groupCells: true
-    }
 
     return (
         <Namespace namespace="home">
             <div className="container">
                 <Helmet title="صفحه ‌اصلی"/>
 
-                <div className="jumbotron mt-3" id="abc">
-                    <h5>موفقیت اتفاقی نیست!</h5>
+                <div className="jumbotron mt-3 pb-3" id="abc">
+                    <h5>RSSR Boilderplate</h5>
                     <p className="lead">
-                        برای خلق بهترین‌ها باید بیشتر تلاش کرد، چیزی که ساده به دست بیاد، می‌تونه خیلی ساده هم از دست بره.
+                        Be happy! Life is too short.
+                        <br/>
+                        We are no better than no one,
+                        We are not in competition with anyone,
+                        We want to be the best version of ourselves.
+                        <br/>
+                        <br/>
+                        see <a href="https://github.com/rssr-org/RSSR-Documentation" target="_blank">Documentation</a>
                     </p>
                 </div>
-
-                <Carousel className="slider-wrap" options={carouselOptions}>
-                    {
-                        sampleCarousel.map((img, index) => (
-                            <div className="item" key={index}>
-                                <img src={img} alt="RSSR LOGO"/>
-                            </div>
-                        ))
-                    }
-                </Carousel>
-
-                <Breakpoint from="md">
-                    {
-                        () => (
-                            <div className="alert alert-info">مثالی برای Breakpoint</div>
-                        )
-                    }
-                </Breakpoint>
 
                 <div>
                     {
@@ -76,20 +48,20 @@ function Home(props) {
                     {
                         (homepage.isLoading) ?
                             (
-                                <div className="col-24 text-center">
+                                <div className="col-6 text-center">
                                     <img src="/asset/img/loading.gif" alt="loading"/>
-                                    <div> در حال بار گذاری مطالب</div>
+                                    <div> fetching data ...</div>
                                 </div>
                             )
                             :
                             (
                                 homepage.map((item) => (
-                                    <div className="col-md-8 my-2 px-3" key={item.id}>
+                                    <div className="col-md-4 my-2 px-3" key={item.id}>
                                         <Link to={route.post(item.id)} className="card">
                                             <div className="card-body">
                                                 <h3 className="card-title text-truncate h6">{item.title}</h3>
                                                 <p className="card-text text-truncate">{item.body}</p>
-                                                <span>مشاهده مطلب</span>
+                                                <span>See</span>
                                             </div>
                                         </Link>
                                     </div>
