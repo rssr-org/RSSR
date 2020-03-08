5caae2c103c95f60aa30d52552cab0d2819bc475
ebrahimiaval <m.ebrahimiaval@gmail.com>
2020-03-08 19:58:05 +0330

    refact UI, move from persian to english lang

--- src/App/Error404/Error404.js
+++ src/App/Error404/Error404.js
@@ -5,19 +5,21 @@ import {browserHistory} from "../../setup/browserHistory";
 
 function Error404(props) {
     return (
-        <div className="container-fluid mb-3">
+        <div className="container mb-3">
             <Helmet title="متاسفانه صفحه مورد نظر یافت نشد!"/>
             <div className="row">
-                <div className="col-24 text-center">
-                    <h4 className="py-5">متاسفانه صفحه مورد نظر پیدا نشد!</h4>
+                <div className="col-12 text-center">
+                    <h4 className="py-5">Page Not Found!</h4>
                     <img src="/asset/img/error-404.png" alt="page not found" width="300"/>
                     <br/>
                     <button className="btn btn-secondary ml-3 mt-4" onClick={() => browserHistory.goBack()}>
                         <i className="fa fa-angle-right font-weight-bold ml-1"></i>
-                        بازگشت
+                        Back
                     </button>
                     <div className="mt-3 alert alert-info">
-                        نمونه skeleton: {props.skeleton.title}
+                        example skeleton data
+                        <br/>
+                        {props.skeleton.title}
                     </div>
                 </div>
             </div>
