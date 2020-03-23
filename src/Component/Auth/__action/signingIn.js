352478904d02823215c40a2153627d2563bc330a
Zoha Pourebtehaj <pourebtehaj.z@gmail.com>
2020-03-23 17:08:18 +0430

    Update signingIn.js

--- src/Component/Auth/__action/signingIn.js
+++ src/Component/Auth/__action/signingIn.js
@@ -4,13 +4,13 @@ import Cookies from "js-cookie";
 /**
  * signing in user
  * push token to localStorage (change null to token string) just when user checked "remember me" checkbox.
- * NOTICE: if user do not checked "remember me" stay logged-in Until the page refreshes.
+ * NOTICE: if user did not check the "remember me" checkbox, stay logged-in until the page refreshes.
  *
  * @param token <string>: user authentication key. like "eyJ0eXAiOiJKV1QiLCJhbGciOiJ...."
  * @param rememberMe <boolean>: state of "remember me" checkbox checked
  */
 export const signingIn = (token, rememberMe) => {
-    // set local User Token when rememberMe
+    // set local user token when rememberMe is checked
     if (rememberMe)
         Cookies.set('localUserToken', token)
 
