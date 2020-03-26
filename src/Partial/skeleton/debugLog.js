183dcf57bfa92878cbe51d01845422b02ff8b4b6
Zoha Pourebtehaj <pourebtehaj.z@gmail.com>
2020-03-26 12:54:06 +0430

    Update debugLog.js

--- src/Partial/skeleton/debugLog.js
+++ src/Partial/skeleton/debugLog.js
@@ -1,4 +1,4 @@
-// active switch for debuging logs
+// active switch for debugging logs
 const debug = JSON.parse(process.env.RSSR_SKELETON_DEBUG);
 
 export const debugLog = function (msgKey) {
@@ -8,16 +8,16 @@ export const debugLog = function (msgKey) {
     let msg;
     switch (msgKey) {
         case "INVALID_CACHE_VALUE":
-            msg = 'cache property is not number or more than ziro miliseconds. set skeleton cache parameter.';
+            msg = 'cache property is not number or more than zero milliseconds. set skeleton cache parameter.';
             break;
         case "WENT_WELL":
-            msg = 'everything went well, data successfully fetched in Server and received on Client.';
+            msg = 'everything went well, data was successfully fetched in Server and was received by Client.';
             break;
         case "FETCHED_IN_CLIENT":
-            msg = 'fetch errored in server but data successfully fetched in Client.';
+            msg = 'fetch errored in server but data was successfully fetched in Client.';
             break;
         case "CLIENT_ERRORED":
-            msg = 'fetch errored in both side.the default data is now used.';
+            msg = 'fetch had error in both sides.The default data is now used.';
             break;
         case "READ_FROM_CACHE":
             msg = 'read data from cache.';
@@ -32,10 +32,10 @@ export const debugLog = function (msgKey) {
             msg = 'fetching data from API.';
             break;
         case "SUCCESSFULLY_FETCH":
-            msg = 'data fetched successfully in server and pushed to skeleton state.';
+            msg = 'data was fetched successfully in server and pushed to skeleton state.';
             break;
         case "SERVER_ERRORED":
-            msg = 'fetch errored in server.';
+            msg = 'fetch had error in server.';
             break;
         default:
             msg = 'message key is invalid!'
