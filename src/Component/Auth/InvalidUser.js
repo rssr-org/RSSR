54fd115657e0dd7afc1e584dc41f519fef84b129
ebrahimiaval <m.ebrahimiaval@gmail.com>
2020-05-11 13:49:00 +0430

    fix condition bug

--- src/Component/Auth/InvalidUser.js
+++ src/Component/Auth/InvalidUser.js
@@ -2,7 +2,7 @@ import {connect}     from "trim-redux";
 import {isValidUser} from "../../setup/utility/isValidUser";
 
 const InvalidUser = props => {
-    return !isValidUser() ? props.children : '';
+    return !isValidUser(false) ? props.children : '';
 }
 
 export default connect(s => ({user: s.user}))(InvalidUser);
